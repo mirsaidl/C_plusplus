19cfd6b5e11dc35f92c1f3a4eecb5cbfa0a434d1
Mirsaid Abdurasulov <145886003+mirsaidl@users.noreply.github.com>
2023-10-10 23:47:50 +0900

    Add files via upload

---
+++ Homework 1 Lab 1/Input and Search/Exercise1.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+
+void inputArray(int* buf, int size) {
+	for (int i = 0; i < size; i++) {
+		cin >> buf[i];
+	}
+}
+int searchIndex(int* buf, int size, int element) {
+	for (int i = 0; i < size; i++) {
+		if (buf[i] == element)
+			return i;
+	}
+	
+	return -1;
+}
+
+int main() {
+	int size, element, index;
+	int* buf = nullptr;
+	cout << "Enter size of array: ";
+	cin >> size;
+	// 1. allocate the memory buffer
+	buf = new int[size];
+
+	// 2. use the allocated memory
+	if (buf == nullptr) {
+		cout << "Memory allocation failed.";
+		return 1;
+	}
+
+
+	cout << "Enter elements of the array: ";
+	inputArray(buf, size);
+	cout << "Enter element to search: ";
+	cin >> element;
+	index = searchIndex(buf, size, element);
+	if (index == -1)
+		cout << element << " does not exist in the array.";
+	else
+		cout << element << " is found in index " << index << '.';
+	// 3. free the allocated memory
+
+	delete[] buf;
+
+	return 0;
+}
+
